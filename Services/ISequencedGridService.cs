f4e678d8edeaa8ce323921caeae444e97d559e89
Darren166 <darren.hall@cognim.co.uk>
2015-09-12 19:05:47 +0100

    Added untracked files

---
+++ Services/ISequencedGridService.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace Services
+{
+    public interface ISequencedGridService
+    {
+        Services.Models.GameGrid Create();
+    }
+}
---
+++ Services/SequencedGridService.cs
@@ -0,0 +1,21 @@
+﻿using Services.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class SequencedGridService : ISequencedGridService 
+    {
+        public GameGrid Create(){
+            GameGrid gameGrid = new GameGrid();
+            for (int id = 1; id < 10; id++)
+            {
+                gameGrid.Cells.Add(new Cell { Id = id });
+            }
+            return gameGrid;
+        }
+    }
+}
