850ed2c7a2b2c0c7d38bb9d106d8d62650d71988
Darren166 <darren.hall@cognim.co.uk>
2015-09-14 19:29:18 +0100

    Added basic display

--- QuickGames/Scripts/GameGrids/SequencedGameGrid.js
+++ QuickGames/Scripts/GameGrids/SequencedGameGrid.js
@@ -3,9 +3,26 @@
 (function () {
 	"use strict";
 
-	function sequencedGridController() {
+	var sequencedGridController = function ($http) {
 		var gridViewModel = this;
 		gridViewModel.Name = "Hello";
-	}
+
+		var chunkArray = function (array, size) {
+			var chunkedArray = [];
+
+			for (var i = 0, len = array.length; i < len; i += size) {
+				chunkedArray.push(array.slice(i, i + size));
+			}
+			return chunkedArray;
+		}
+
+		var onGotGrid = function (response) {
+			gridViewModel.grid = response.data;
+			gridViewModel.grid.rows = chunkArray(gridViewModel.grid.Cells, gridViewModel.grid.NumberOfColumns);
+		}
+
+		$http.get("http://localhost:53246/api/GridSequenceGame?rows=2&columns=2").then(onGotGrid);
+	};
+
 	angular.module('gameGrid').controller('sequencedGridController', sequencedGridController);
 }());
