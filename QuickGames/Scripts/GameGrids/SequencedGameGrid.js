0eb7562e0cc46465838e2c7e3f0f750b01845feb
Darren166 <darren.hall@cognim.co.uk>
2015-09-14 13:29:32 +0100

    Began adding angular

---
+++ QuickGames/Scripts/GameGrids/SequencedGameGrid.js
@@ -0,0 +1,11 @@
+﻿angular.module('gameGrid', []);
+
+(function () {
+	"use strict";
+
+	function sequencedGridController() {
+		var gridViewModel = this;
+		gridViewModel.Name = "Hello";
+	}
+	angular.module('gameGrid').controller('sequencedGridController', sequencedGridController);
+}());
