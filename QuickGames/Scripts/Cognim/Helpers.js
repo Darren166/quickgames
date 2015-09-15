e8f3e83e62671abfcb3c7fb360c4f0488f8ce319
Darren166 <darren.hall@cognim.co.uk>
2015-09-15 17:45:02 +0100

    Added click function and helper class

---
+++ QuickGames/Scripts/Cognim/Helpers.js
@@ -0,0 +1,17 @@
+﻿(function (Cognim) {
+	"use strict";
+
+	Cognim.Helpers = Cognim.Helpers || {
+
+		chunkArray: function (array, size) {
+			var chunkedArray = [];
+
+			for (var i = 0, len = array.length; i < len; i += size) {
+				chunkedArray.push(array.slice(i, i + size));
+			}
+			return chunkedArray;
+		},
+
+	};
+
+}(window.Cognim = window.Cognim || {}));
---
+++ QuickGames/Scripts/GameGrids/SequencedGridController.js
@@ -0,0 +1,36 @@
+﻿angular.module('gameGrid', []);
+
+(function () {
+	"use strict";
+
+	var sequencedGridController = function ($http) {
+
+		var gridViewModel = this;
+
+		var getCellById = function(id){
+			for (var index = 0; index < gridViewModel.grid.Cells.length; index++) {
+				if (gridViewModel.grid.Cells[index].Id === id) {
+					return gridViewModel.grid.Cells[index];
+				}
+			}
+			return null;
+		}
+
+		gridViewModel.onCellClicked = function (cell) {
+			if (cell == gridViewModel.expectedCell) {
+				gridViewModel.expectedCell = getCellById(cell.Id + 1);
+			}
+			
+		}
+
+		var onGotGrid = function (response) {
+			gridViewModel.grid = response.data;
+			gridViewModel.grid.rows = Cognim.Helpers.chunkArray(gridViewModel.grid.Cells, gridViewModel.grid.NumberOfColumns);
+			gridViewModel.expectedCell = getCellById(1);
+		}
+
+		$http.get("http://localhost:53246/api/GridSequenceGame?rows=4&columns=4").then(onGotGrid);
+	};
+
+	angular.module('gameGrid').controller('sequencedGridController', sequencedGridController);
+}());
